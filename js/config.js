6c9828f5f98c29628de98e5ad0b5605cfffb225a
Michal Durinik <misko.durinik@gmail.com>
2019-06-26 15:40:50 +0100

    fixing region

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-1_fhSoo78pb', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6n6hpq19km86mn7dsvl18rei2q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'eu-west-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };
