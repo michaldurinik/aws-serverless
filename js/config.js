8805221c836c4d1a3010be1ab9ea80352f5ff427
Michal Durinik <misko.durinik@gmail.com>
2019-06-26 15:03:25 +0100

    updating user config pools

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_Q6RMOXdzL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ov8ct3i8er8qsbp6p7jgulc5l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'eu-west-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };
